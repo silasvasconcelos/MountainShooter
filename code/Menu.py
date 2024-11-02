fd6aa94a7c1ce86fd050b31a247f6bce300ccba4
Silas Vasconcelos <silasvasconcelos@hotmail.com.br>
2024-11-02 18:39:09 -0300

    Add student name and register number

--- code/Menu.py
+++ code/Menu.py
@@ -24,6 +24,8 @@ class Menu:
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
+            self.menu_text(20, "Silas Vasconcelos - 4548512", C_WHITE, (WIN_WIDTH - 100, WIN_HEIGHT - 15))
+
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
                     self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
